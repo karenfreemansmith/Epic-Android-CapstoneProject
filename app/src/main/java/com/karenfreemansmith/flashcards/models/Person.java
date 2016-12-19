a36a9816eac1afaa816bae18cad1d5b8619ba21d
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-12-19 10:58:36 -0800

    create data model for people

---
+++ app/src/main/java/com/karenfreemansmith/flashcards/models/Person.java
@@ -0,0 +1,87 @@
+package com.karenfreemansmith.flashcards.models;
+
+/**
+ * Created by Guest on 12/19/16.
+ */
+public class Person {
+    private String mCountry;
+    private String mFlag;
+    private String mHistory;
+    private String mMap;
+    private String mName;
+    private String mOffice;
+    private String mPhoto;
+    private String mTitle;
+
+    public String getmCountry() {
+        return mCountry;
+    }
+
+    public void setmCountry(String mCountry) {
+        this.mCountry = mCountry;
+    }
+
+    public String getmFlag() {
+        return mFlag;
+    }
+
+    public void setmFlag(String mFlag) {
+        this.mFlag = mFlag;
+    }
+
+    public String getmHistory() {
+        return mHistory;
+    }
+
+    public void setmHistory(String mHistory) {
+        this.mHistory = mHistory;
+    }
+
+    public String getmMap() {
+        return mMap;
+    }
+
+    public void setmMap(String mMap) {
+        this.mMap = mMap;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmOffice() {
+        return mOffice;
+    }
+
+    public void setmOffice(String mOffice) {
+        this.mOffice = mOffice;
+    }
+
+    public String getmPhoto() {
+        return mPhoto;
+    }
+
+    public void setmPhoto(String mPhoto) {
+        this.mPhoto = mPhoto;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Person(String name) {
+        mName = name;
+
+    }
+
+
+
+}
