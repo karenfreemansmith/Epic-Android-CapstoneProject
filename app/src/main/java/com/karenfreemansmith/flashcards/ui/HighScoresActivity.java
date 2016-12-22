79d11848a0512570d9537e09cd4e11cbffd8c016
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-12-22 12:05:43 -0800

    improve layout some more

--- app/src/main/java/com/karenfreemansmith/flashcards/ui/HighScoresActivity.java
+++ app/src/main/java/com/karenfreemansmith/flashcards/ui/HighScoresActivity.java
@@ -41,8 +41,8 @@ public class HighScoresActivity extends AppCompatActivity {
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mEditor = mSharedPreferences.edit();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        mUser = user.getDisplayName();
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        mUser = user.getDisplayName();
 
         mScore = mSharedPreferences.getString(Constants.PREFERENCES_SCORE_KEY, null);
         mTotal = mSharedPreferences.getString(Constants.PREFERENCES_TOTAL_KEY, null);
@@ -102,6 +102,30 @@ public class HighScoresActivity extends AppCompatActivity {
     public void playAgain() {
         Intent intent = new Intent(HighScoresActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.buttonReset)
+    public void reset() {
+        for(int i=0; i<mHighScores.length; i++) {
+            mHighScores[i]="0.001";
+        }
+
+        // display scores with list adapter
+        mListView = (ListView) findViewById(R.id.listView);
+        HighScoreAdapter adapter = new HighScoreAdapter(this, android.R.layout.simple_list_item_1, mHighScores, mUser);
+        mListView.setAdapter(adapter);
+
+        // save new scores to shared preferences
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE01_KEY, mHighScores[0]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE02_KEY, mHighScores[1]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE03_KEY, mHighScores[2]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE04_KEY, mHighScores[3]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE05_KEY, mHighScores[4]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE06_KEY, mHighScores[5]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE07_KEY, mHighScores[6]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE08_KEY, mHighScores[7]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE09_KEY, mHighScores[8]).apply();
+        mEditor.putString(Constants.PREFERENCES_HIGHSCORE10_KEY, mHighScores[9]).apply();
     }
 
     @OnClick(R.id.buttonLogout)
